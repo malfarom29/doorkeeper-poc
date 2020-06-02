07885633342f61b0b5ad6ccbb882a0af57f5283d
Manuel Alfaro <malfaro@applaudostudios.com>
2020-06-01 18:13:08 -0600

    ✨ Add Faraday base connection class

---
+++ app/services/faraday_connection/base_connection.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module FaradayConnection
+  class BaseConnection
+    class_attribute :url
+
+    def initialize(**)
+      @api = api
+    end
+
+    def api
+      Faraday.new(url: url) do |f|
+        f.request :multipart
+        f.request :url_encoded
+        f.adapter :net_http
+      end
+    end
+
+    def request(method, options = {})
+      response = @api.public_send(method, options.fetch(:path, '')) do |req|
+        req.params.merge!(options[:query])
+        req.body = options[:body].to_json if options[:body]
+      end
+      response
+    end
+  end
+end
