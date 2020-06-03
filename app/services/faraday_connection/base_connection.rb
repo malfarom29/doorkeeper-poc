812bb53841f74697ff2019af111c1ddf3a830d93
Manuel Alfaro <malfaro@applaudostudios.com>
2020-06-03 00:00:34 -0600

    🎨 Refactor BaseConnection class

--- app/services/faraday_connection/base_connection.rb
+++ app/services/faraday_connection/base_connection.rb
@@ -4,21 +4,18 @@ module FaradayConnection
   class BaseConnection
     class_attribute :url
 
-    def initialize(**)
-      @api = api
-    end
-
-    def api
-      Faraday.new(url: url) do |f|
-        f.request :multipart
-        f.request :url_encoded
-        f.adapter :net_http
-      end
+    def initialize
+      @api =
+        Faraday.new(url: url) do |f|
+          f.request :multipart
+          f.request :url_encoded
+          f.adapter :net_http
+        end
     end
 
     def request(method, options = {})
       response = @api.public_send(method, options.fetch(:path, '')) do |req|
-        req.params.merge!(options[:query])
+        req.params.merge!(options[:query]) if options[:query]
         req.body = options[:body].to_json if options[:body]
       end
       response
